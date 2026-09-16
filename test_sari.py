ac3dddc839bd03f3bfd3de27d1529bff37f3f334
Samet710 <samet2135byr@gmail.com>
2026-09-16 14:34:48 +0300

    Create test_sari.py

---
+++ test_sari.py
@@ -0,0 +1,35 @@
+from sari_sistem import Oyuncu
+
+
+def test_oyuncu_olusturma():
+    oyuncu = Oyuncu()
+
+    assert oyuncu.isim == "AllStar"
+    assert oyuncu.seviye == 1
+    assert oyuncu.xp == 0
+    assert oyuncu.envanter == []
+
+
+def test_xp_kazanma():
+    oyuncu = Oyuncu()
+
+    oyuncu.xp_kazan(50)
+
+    assert oyuncu.xp == 50
+
+
+def test_envanter_baslangici():
+    oyuncu = Oyuncu()
+
+    assert isinstance(
+        oyuncu.envanter,
+        list
+    )
+
+
+def test_seviye_atlama():
+    oyuncu = Oyuncu()
+
+    oyuncu.xp_kazan(120)
+
+    assert oyuncu.seviye >= 2
